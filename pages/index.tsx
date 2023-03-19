40d2cb82e986702d63db0846111aef0c76c2c411
Lloyd Lobo <lloydlobo4@gmail.com>
2023-03-19 18:37:25 +0530

    style: change user icon to moon theme toggle wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -120,9 +120,9 @@ export default function Home() {
 				{loading === false && results !== null ?
 					<Results results={results} keyword={search} />
 					: (
-					// Removed search.length>0 for intial search at load.
-						 loading ?
-						// search.length > 0 && loading ?
+						// Removed search.length>0 for intial search at load.
+						loading ?
+							// search.length > 0 && loading ?
 							<SkeletonLayout />
 							: <div>&nbsp;</div>
 					)
@@ -379,26 +379,18 @@ export function Navbar() {
 							<div className="absolute inset-y-0 right-0 rounded-full flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 								<button
 									type="button"
-									className="rounded-full bg-gray-800  p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+									className="  hidden rounded-full bg-gray-800  p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 								>
 									<span className="sr-only">View notifications</span>
 									<SearchIcon className="h-6 w-6" aria-hidden="true" />
 								</button>
 
 								{/* Profile dropdown */}
-								<Menu as="div" className="relative ml-3">
+								<Menu as="div" className="relative blur-[1px] pointer-events-none ml-3">
 									<div className="h-8 relative w-8">
 										<Menu.Button className="rounded-full inset-0 relative h-full w-full grid active:outline-white">
-											<span className="sr-only">Open user menu</span>
-											<Image
-												width={32}
-												height={32}
-												loader={myLoader}
-												// placeholder="blur"
-												className="h-8 w-8 absolute -top-[100%] focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 rounded-full"
-												src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-												alt=""
-											/>
+											<span className="hidden">Open user menu</span>
+											<MoonIcon />
 										</Menu.Button>
 									</div>
 									<Transition
@@ -494,3 +486,25 @@ const LinkIcon = () => {
 		</>
 	)
 }
+
+const MoonIcon = () => {
+	return (
+		<>
+
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+				<path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+			</svg>
+		</>
+	)
+}
+const SunIcon = () => {
+	return (
+		<>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+				<path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+			</svg>
+		</>
+	)
+}
+
+
